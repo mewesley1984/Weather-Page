bc166b65800cbe5ca7e1f2ce3c828b62ba37aa1a
Matt  Wesley <mattwesley@Matts-MacBook-Air.local>
2023-04-13 16:28:42 -0400

    updated README with link

--- assets/script.js
+++ assets/script.js
@@ -22,30 +22,15 @@ fetch(apiKey)
 }
 
 function displayForecast(data){
-    var tommorrowEl = document.getElementById("tomorrow")
-    tommorrowEl.textcontent = data.list[8].main.temp
-    
-    var daytwoEl = document.getElementById("day"+ 2)
-    var daythreeEl = document.getElementById("day3")
-
-    daytwoEl.textcontent = data.list[16].main.temp
-    
-    daythreeEl.textcontent= data.list[24].main.temp
-    
-    
-    
-    
-    console.log("Tomorrow >>>",data.list[8].main.temp ) //4/14
-    console.log("Day after >>>", data.list[24].main.temp) //4/16
-    console.log("Day after >>>", data.list[32].main.temp) //4/17
-    console.log("Day after >>>", data.list[40].main.temp) //4/18
 
     //for(VARIABLE; CONDITION; AFTER)
     var day = 1
     for (var i=8; i <= 40; i += 8) {
         console.log("Day after >>>", data.list[i].main.temp)
         var currentTempEl = document.getElementById("day"+ day+"temp")
-        currentTempEl = data.list[i].main.temp
+        currentTempEl.textContent = data.list[i].main.temp
+        var currentWindEl= document.getElementById("day" + day +"wind") 
+        currentWindEl.textContent = data.list[i].wind.speed
         var currentHumidEl = document.getElementById("day"+ day+"humid")
         currentHumidEl.textContent = data.list[i].main.humidity
         day++
@@ -55,10 +40,10 @@ function displayForecast(data){
 
 function displayCurrent(data){
     var weather = data.list[0]
-    currentCityEl.textContent= data.city.name
-    currentTempEl.textContent="Current Temperature:"  +  weather.main.temp 
-    currentWindEl.textContent="Wind Speed:" +  weather.wind.speed
-    currentHumidityEl.textContent="Humidity:"  +  weather.main.humidity 
+    currentCityEl.textContent= weather.city.name
+    currentTempEl.textContent="Current Temperature: "  +  weather.main.temp 
+    currentWindEl.textContent="Wind Speed: " +  weather.wind.speed
+    currentHumidityEl.textContent="Humidity: "  +  weather.main.humidity 
 
 }
 
