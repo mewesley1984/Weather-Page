dfc24d14487d567d29cab281b1c5c75cb0876485
Matt  Wesley <mattwesley@Matts-MacBook-Air.local>
2023-04-16 22:06:27 -0400

    added JS and html code for local storage and forecast

--- assets/script.js
+++ assets/script.js
@@ -4,54 +4,103 @@ var currentCityEl=document.getElementById("current-weather-city")
 var currentTempEl=document.getElementById("current-weather-temp")
 var currentWindEl=document.getElementById("current-weather-wind")
 var currentHumidityEl=document.getElementById("current-weather-humidity")
+var forecastEl = document.getElementById("forecast")
+var savedCitiesEl= document.getElementById("saved-cities")
 
 function getWeather(city) {
-    
     var apiKey=`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=7a86c802b697310f6f3bd72dc88c3de5&units=imperial`
 
-fetch(apiKey)
-.then(function(response){
-    return response.json();
-})
-.then(function (data) {
-    console.log(data)
-    displayCurrent(data)
-    displayForecast(data)
-})
+    fetch(apiKey)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data)
+        displayCurrent(data)
+        displayForecast(data)
+        return data
+    })
+}
 
+function getAndSaveWeather(city) {
+    var apiKey=`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=7a86c802b697310f6f3bd72dc88c3de5&units=imperial`
+
+    fetch(apiKey)
+        .then(function(response){
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data)
+            displayCurrent(data)
+            displayForecast(data)
+            return data
+        })
+        .then(data => {
+            var fromstorage = localStorage.getItem('saved-cities') || "[]"
+            var savedCities = JSON.parse(fromstorage)
+            savedCities.push(data.city.name)
+            localStorage.setItem('saved-cities', JSON.stringify(savedCities))
+            return savedCities
+        })
+        .then(cities => {
+            displaySavedCities(cities)
+        })
+}
+
+function displaySavedCities(cities) {
+
+    //const myArr = ['milwaukee', 'green bay']
+
+    var buttons = cities.map((item) =>
+        `<p><button onclick="getWeather('${item}')">${item}</button></p>`
+    )
+
+// ['<button></button>', '<button></button>']
+
+// '<button></button><button></button>'
+
+   savedCitiesEl.innerHTML = buttons.join('')
 }
 
 function displayForecast(data){
 
     //for(VARIABLE; CONDITION; AFTER)
-    var day = 1
+    forecastEl.innerHTML = '<h1>5-Day Forecast:</h1>'
+    
     for (var i=7; i <= 40; i += 8) {
-        console.log("Day after >>>", data.list[i].main.temp)
-        var currentTempEl = document.getElementById("day"+ day+"temp")
-        currentTempEl.textContent = data.list[i].main.temp
-        var currentWindEl= document.getElementById("day" + day +"wind") 
-        currentWindEl.textContent = data.list[i].wind.speed
-        var currentHumidEl = document.getElementById("day"+ day+"humid")
-        currentHumidEl.textContent = data.list[i].main.humidity
-        day++
+        var day = data.list[i]
+        var formattedDate = new Date(day.dt_txt).toLocaleDateString()
+        var weather = day.weather?.[0].description
+        var dayForecastHtml = `
+        <div>
+            <h2 class="date">${formattedDate}</h2>
+            <p>${weather}</p> 
+            <label>Temp:</label> <h4 class="temp">${day.main.temp}℉</h4>
+            <label>Wind:</label> <h4 class="wind">${day.wind.speed} MPH</h4>
+            <label>Humidity:</label> <h4 class="humidity">${day.main.humidity}%</h4>
+        </div>
+        `
+        forecastEl.innerHTML += dayForecastHtml
     }
 
 }
 
 function displayCurrent(data){
+    if(!data) {return}
+
     var weather = data.list[0]
-    currentCityEl.textContent= data.city.name
-    currentTempEl.textContent="Current Temperature: "  +  weather.main.temp 
-    currentWindEl.textContent="Wind Speed: " +  weather.wind.speed
-    currentHumidityEl.textContent="Humidity: "  +  weather.main.humidity 
+    var date = new Date().toLocaleDateString()
+    currentCityEl.textContent= data.city.name + ": " + date
+    currentTempEl.textContent="Current Temperature: "  +  weather.main.temp + "℉" 
+    currentWindEl.textContent="Wind Speed: " +  weather.wind.speed + " MPH"
+    currentHumidityEl.textContent="Humidity: "  +  weather.main.humidity + "%"
 
 }
 
 function searchResult(event) {
     event.preventDefault();
-
-console.log (searchFormEl.value)
-getWeather(searchFormEl.value)
+    console.log (searchFormEl.value)
+    getAndSaveWeather(searchFormEl.value)
 }
 
 
